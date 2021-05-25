a5e97930bd9aea16e6db01b13fccb140e6270f3d
Neeraj Yadav <ny29599@gmail.com>
2021-05-25 21:06:18 +0530

    📦 NEW: add data persists

--- utils/ask.js
+++ utils/ask.js
@@ -1,16 +1,37 @@
 const fs = require('fs')
+const os = require('os')
+const path = require('path')
 const { Input } = require('enquirer')
 const to = require('await-to-js').default
 const handleError = require('cli-handle-error')
-const shouldCancel = require('cli-should-cancel');
+const shouldCancel = require('cli-should-cancel')
+const { Store } = require('data-store')
 
 module.exports = async ({ name, message, hint, initial }) => {
+  let history = false;
+  if (
+    !initial &&
+    name !== `name` &&
+    name !== `command` &&
+    name !== `description`
+  ) {
+    history = {
+      autosave: true,
+      store: new Store({
+        path: path.join(
+          os.homedir(),
+          `.history/create-node-cli/${name}.json`
+        )
+      })
+    };
+  }
   const [err, response] = await to(
     new Input({
       name,
       message,
       hint,
       initial,
+      history,
       validate(value, state) {
         if (state && state.name === `command`) return true;
         if (state && state.name === `name`) {
