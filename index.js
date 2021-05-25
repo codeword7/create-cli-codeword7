e448396b9d9a6b3d20b8f33630ac071db2ce540a
Neeraj Yadav <ny29599@gmail.com>
2021-05-25 12:12:42 +0530

    📦 NEW: template for all personal cli

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const start = async () => {
   init()
 
   const name = await ask({ message: `CLI name?`, hint: `(use kabab-case only)` })
+  const command = await ask({ message: `CLI command?`, hint: `(optional: if different from CLI name)` })
   const description = await ask({ message: `CLI description?` })
-  const version = await ask({ message: `CLI name?`, initial: `0.0.1` })
+  const version = await ask({ message: `CLI version?`, initial: `0.0.1` })
   const lincense = await ask({ message: `CLI license?`, initial: `UNLICENSED` })
   const authorName = await ask({ message: `CLI author name?` })
   const authorEmail = await ask({ message: `CLI author email?` })
-  const authorUrl = await ask({ message: `CLI author url?` })
+  const authorUrl = await ask({ message: `CLI author URL?` })
 
   const vars = {
     name,
+    command: command ? command : name,
     description,
     version,
     lincense,
