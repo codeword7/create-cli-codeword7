f1c9e5a4455d253350464d97f652c246021f9437
Neeraj Yadav <ny29599@gmail.com>
2021-05-25 19:46:54 +0530

    📦 NEW: create cli without duplicates

--- utils/generate.js
+++ utils/generate.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const execa = require('execa');
 const copy = require('copy-template-dir')
 const alert = require('cli-alerts-codeword7');
 const { green: g, dim: d } = require('chalk')
@@ -11,13 +12,16 @@ module.exports = async () => {
   const inDir = path.join(__dirname, `../template`)
   const outDir = path.join(process.cwd(), output)
 
-  copy(inDir, outDir, vars, (err, createdFiles) => {
+  copy(inDir, outDir, vars, async (err, createdFiles) => {
     if (err) throw err;
     console.log(d(`\nCreating files in ${g(`./${output}`)} directory:\n`))
     createdFiles.forEach(filePath => {
       const fileName = path.basename(filePath)
       console.log(`${g(`CREATED`)} ${fileName}`);
     })
+
+    process.chdir(outDir)
+    await execa(`npm`, [`dedupe`])
     alert({
       type: `success`,
       name: `All Done`,
--- utils/questions.js
+++ utils/questions.js
@@ -2,7 +2,7 @@ const ask = require('./ask');
 
 const questions = async () => {
   const name = await ask({ message: `CLI name?`, hint: `(use kabab-case only)` })
-  const command = await ask({ message: `CLI command?`, hint: `(optional: if different from CLI name)` })
+  const command = await ask({ message: `CLI command?`, hint: `(optional: if different from CLI name)`, initial: name })
   const description = await ask({ message: `CLI description?` })
   const version = await ask({ message: `CLI version?`, initial: `0.0.1` })
   const lincense = await ask({ message: `CLI license?`, initial: `UNLICENSED` })
